1ef63af1232275e40b56b6221e273a56247df7be
alakuntlavasu <alakuntlavasu2020@gmail.com>
2023-08-21 12:22:28 +0530

    java

---
+++ src/CollectionsFremework/LinkedHashsetExample1.java
@@ -0,0 +1,19 @@
+package CollectionsFremework;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LinkedHashsetExample1 {
+
+	public static void main(String[] args) {
+		
+     Set<Integer> m=new LinkedHashSet<Integer>();
+     
+     m.add(20);
+     m.add(45);
+     m.add(36);
+     
+     System.out.println(m);
+	}
+
+}
