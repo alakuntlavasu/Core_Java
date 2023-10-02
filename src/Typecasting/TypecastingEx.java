d8f6ad3b5a22cb55154a8c57b7a62500fab45bc1
alakuntlavasu <alakuntlavasu2020@gmail.com>
2023-10-02 17:18:36 +0530

    okk

---
+++ src/Typecasting/TypecastingEx.java
@@ -0,0 +1,21 @@
+package Typecasting;
+
+public class TypecastingEx {
+
+	public static void main(String[] args) 
+	{
+	// implcit type casting
+		// lower datatype to higher datatype
+	byte b=10;
+	int i=b;
+	System.out.println(b+" "+i);
+	System.out.println("implcit casting");
+		
+//		int i=10;
+//		byte b= i;
+//		
+//		System.out.println(i+" "+b);
+
+	}
+
+}
