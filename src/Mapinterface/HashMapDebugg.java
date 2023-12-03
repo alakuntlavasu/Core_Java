c962abdee61e9a25b54bc1d511cfa79dfb321747
alakuntlavasu <alakuntlavasu2020@gmail.com>
2023-12-03 10:12:27 +0530

    okk

---
+++ src/Mapinterface/HashMapDebugg.java
@@ -0,0 +1,20 @@
+package Mapinterface;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDebugg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Map<String, Integer> map=new HashMap<>();
+		
+	
+		map.put("bbb", 2);
+		map.put("ccc", 3);
+		System.out.println(map);
+
+	}
+
+}
