082ea0708190092bcf7f879e1247bb4412ff0ac3
alakuntlavasu <alakuntlavasu2020@gmail.com>
2024-01-19 19:17:47 +0530

    evining

--- src/polymorpshim/CompiletimepolymorpshimEx.java
+++ src/polymorpshim/CompiletimepolymorpshimEx.java
@@ -1,9 +1,28 @@
 package polymorpshim;
 
+class Test{
+	
+	static  void m1() {
+		System.out.println("super class method m1()");
+	}
+}
+
+class Demo extends Test
+{
+	
+	static void m1() 
+	{
+		System.out.println("child class method m1()");
+	}
+}
+
 public class CompiletimepolymorpshimEx {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
+		Test t=new Demo();
+		t.m1();
 
 	}
 
