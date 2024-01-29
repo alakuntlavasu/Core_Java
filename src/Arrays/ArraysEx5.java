a3c3be8fd4cd69b0fa2f74859063ed27a0965c1e
alakuntlavasu <alakuntlavasu2020@gmail.com>
2024-01-29 15:08:20 +0530

    okk

---
+++ src/Arrays/ArraysEx5.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ArraysEx5 {
+
+	public static void main(String[] args) {
+	
+		int[] a= {30,202,10};
+		  
+		int sum=0;
+		
+		for(int i=0; i<a.length; i++) {
+			
+			sum =sum+a[i];
+			
+		}	
+		System.out.println(sum);
+		
+	
+
+	}
+
+}
