c11e76abe3e98f2171e521c5be347ab83f548898
alakuntlavasu <alakuntlavasu2020@gmail.com>
2024-03-08 21:56:18 +0530

    strings

---
+++ src/Strings/Demo3.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char[] chr= {'a','b','t','f','d'};
+		
+		String str=new String(chr);
+		
+		String v= String.valueOf(chr[4]);
+		System.out.println(v);
+
+	}
+
+}
